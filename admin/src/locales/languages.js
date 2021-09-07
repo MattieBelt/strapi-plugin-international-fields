c1c9dd968f032f125d6606f2939e45d74959f69f
Mattias van den Belt <mattias@taskhero.nl>
2021-09-07 11:43:14 +0200

    Add id, ms, th, and vi Languages locales

--- admin/src/locales/languages.js
+++ admin/src/locales/languages.js
@@ -3,12 +3,16 @@ import de from '@cospired/i18n-iso-languages/langs/de.json';
 import en from '@cospired/i18n-iso-languages/langs/en.json';
 import es from '@cospired/i18n-iso-languages/langs/es.json';
 import fr from '@cospired/i18n-iso-languages/langs/fr.json';
+import id from '@cospired/i18n-iso-languages/langs/id.json';
 import it from '@cospired/i18n-iso-languages/langs/it.json';
 import ja from '@cospired/i18n-iso-languages/langs/ja.json';
+import ms from '@cospired/i18n-iso-languages/langs/ms.json';
 import nl from '@cospired/i18n-iso-languages/langs/nl.json';
 import pl from '@cospired/i18n-iso-languages/langs/pl.json';
 import pt from '@cospired/i18n-iso-languages/langs/pt.json';
 import ru from '@cospired/i18n-iso-languages/langs/ru.json';
+import th from '@cospired/i18n-iso-languages/langs/th.json';
+import vi from '@cospired/i18n-iso-languages/langs/vi.json';
 import zh from '@cospired/i18n-iso-languages/langs/zh.json';
 
 const languages = {
@@ -17,13 +21,17 @@ const languages = {
   en,
   es,
   fr,
+  id,
   it,
   ja,
+  ms,
   nl,
   pl,
   'pt-BR': pt,
   pt,
   ru,
+  th,
+  vi,
   'zh-Hans': zh,
   zh,
 };
